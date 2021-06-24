eeeb3e7f098f56a265bf531fd6dfc6697321dc1b
Steve <stevebotella@hotmail.fr>
2021-06-24 17:10:42 +0200

    feat: edit fields with fk

--- sql/llx_categorie_piste.sql
+++ sql/llx_categorie_piste.sql
@@ -1,5 +1,5 @@
 CREATE TABLE llx_c_xsport_type (
     rowid int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    categorie_id integer,
-    piste_id integer
+    fk_categorie_id integer,
+    fk_piste_id integer
 )
