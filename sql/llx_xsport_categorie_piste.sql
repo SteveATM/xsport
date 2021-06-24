4282a7bcf16bee82e8696640238480c0b1c6599d
Steve <stevebotella@hotmail.fr>
2021-06-24 17:19:51 +0200

    fix: Rename table

--- sql/llx_xsport_categorie_piste.sql
+++ sql/llx_xsport_categorie_piste.sql
@@ -1,4 +1,4 @@
-CREATE TABLE llx_categorie_piste (
+CREATE TABLE llx_xsport_categorie_piste (
     rowid int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     fk_categorie_id int(11) NOT NULL DEFAULT '0',
     fk_piste_id int(11) NOT NULL DEFAULT '0'
