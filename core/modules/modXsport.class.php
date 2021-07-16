45893c11e2526c68cba187e9abf05c48686ffb97
Steve <stevebotella@hotmail.fr>
2021-07-16 14:49:26 +0200

    fix: display left menu

--- core/modules/modXsport.class.php
+++ core/modules/modXsport.class.php
@@ -412,14 +412,34 @@ class modXsport extends DolibarrModules
 		);
 		*/
 
+		$this->menu[$r++]=array(
+			// '' if this is a top menu. For left menu, use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
+			'fk_menu'=>'fk_mainmenu=xsport',
+			// This is a Left menu entry
+			'type'=>'left',
+			'titre'=>'Pistes',
+			'mainmenu'=>'xsport',
+			'leftmenu'=>'xsport_piste',
+			'url'=>'/xsport/xsportindex.php',
+			// Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
+			'langs'=>'xsport@xsport',
+			'position'=>1100+$r,
+			// Define condition to show or hide menu entry. Use '$conf->xsport->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
+			'enabled'=>'$conf->xsport->enabled',
+			// Use 'perms'=>'$user->rights->xsport->level1->level2' if you want your menu with a permission rules
+			'perms'=>'1',
+			'target'=>'',
+			// 0=Menu for internal users, 1=external users, 2=both
+			'user'=>2,
+		);
         $this->menu[$r++]=array(
             // '' if this is a top menu. For left menu, use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
-            'fk_menu'=>'fk_mainmenu=xsport',
+			'fk_menu'=>'fk_mainmenu=xsport,fk_leftmenu=xsport_piste',
             // This is a Left menu entry
             'type'=>'left',
-            'titre'=>'Liste des Pistes',
+            'titre'=>'Liste',
             'mainmenu'=>'xsport',
-            'leftmenu'=>'xsport_piste',
+            'leftmenu'=>'xsport',
             'url'=>'/xsport/piste_list.php',
             // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
             'langs'=>'xsport@xsport',
@@ -439,7 +459,7 @@ class modXsport extends DolibarrModules
             'type'=>'left',
             'titre'=>'Nouvelle Piste',
             'mainmenu'=>'xsport',
-            'leftmenu'=>'xsport_piste',
+            'leftmenu'=>'xsport',
             'url'=>'/xsport/piste_card.php?action=create',
             // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
             'langs'=>'xsport@xsport',
@@ -460,7 +480,7 @@ class modXsport extends DolibarrModules
 			'titre'=>'Nouvelle catégorie de piste',
 			'mainmenu'=>'xsport',
 			'leftmenu'=>'xsport_piste',
-			'url'=>'../categories/card.php?action=create',
+			'url'=>'../admin/dict.php?id=43',
 			// Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
 			'langs'=>'xsport@xsport',
 			'position'=>1100+$r,
