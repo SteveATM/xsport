ad4d7e34d25ff23456819a0378afa96ac61ac913
Steve <stevebotella@hotmail.fr>
2021-07-20 14:32:06 +0200

    fix: MentSpe key to translate

--- core/modules/modXsport.class.php
+++ core/modules/modXsport.class.php
@@ -607,7 +607,7 @@ class modXsport extends DolibarrModules
 		// Create extrafields during init
 		include_once DOL_DOCUMENT_ROOT.'/core/class/extrafields.class.php';
 		$extrafields = new ExtraFields($this->db);
-		$result1=$extrafields->addExtraField('mentionspe', "Mention Special", 'varchar', 1, 10, 'propal',      0, 0, '', '', 1, '', 1, 0, '', '', 'xsport@xsport', '$conf->xsport->enabled', 0 , 1);
+		$result1=$extrafields->addExtraField('mentionspe', "MentSpe", 'varchar', 1, 255, 'propal',      0, 0, '', '', 1, '', 1, 0, '', '', 'xsport@xsport', '$conf->xsport->enabled', 0 , 1);
 		//$result1=$extrafields->addExtraField('xsport_myattr1', "New Attr 1 label", 'boolean', 1,  3, 'thirdparty',   0, 0, '', '', 1, '', 0, 0, '', '', 'xsport@xsport', '$conf->xsport->enabled');
 		//$result2=$extrafields->addExtraField('xsport_myattr2', "New Attr 2 label", 'varchar', 1, 10, 'project',      0, 0, '', '', 1, '', 0, 0, '', '', 'xsport@xsport', '$conf->xsport->enabled');
 		//$result3=$extrafields->addExtraField('xsport_myattr3', "New Attr 3 label", 'varchar', 1, 10, 'bank_account', 0, 0, '', '', 1, '', 0, 0, '', '', 'xsport@xsport', '$conf->xsport->enabled');
